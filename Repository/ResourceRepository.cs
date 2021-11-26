914b9287c8acd7eb000f2c6a4f147e47606ca6e1
Karlo2001 <karl.bilsted.michelsen@gmail.com>
2021-11-26 14:37:52 +0100

    Finish ResourceTest constructor and make build work

--- Repository/ResourceRepository.cs
+++ Repository/ResourceRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Repository.ResourceRepo;
+﻿using Repository.Infrastructure.Context;
+
+namespace Repository.ResourceRepo;
 
 public class ResourceRepository
 {
+    private readonly VideoOverflowContext _context;
 
+    public ResourceRepository(VideoOverflowContext context)
+    {
+        _context = context;
+    }
 }
