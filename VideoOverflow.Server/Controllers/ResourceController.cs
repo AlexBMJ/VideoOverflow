3373eb3d3c80a1f3a9514e3eddb851c38a4ef64b
clyt <clyt@itu.dk>
2021-11-30 19:58:27 +0100

    Added some controller code

---
+++ VideoOverflow.Server/Controllers/ResourceController.cs
@@ -0,0 +1,47 @@
+﻿namespace Server.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+    public class ResourceController : ControllerBase
+    {
+        private readonly ILogger<ResourceController> _logger;
+        private readonly IResourceRepository _repository;
+
+        public ResourceController(ILogger<ResourceController> logger, IResourceRepository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IEnumerable<ResourceDTO>> GetAll()
+            => await _repository.GetAll();
+
+        [AllowAnonymous]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(typeof(ResourceDetailsDTO), 200)]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ResourceDetailsDTO>> Get(int id)
+            => (await _repository.Get(id)).ToActionResult();
+
+        [Authorize]
+        [HttpPost]
+        [ProducesResponseType(typeof(ResourceDetailsDTO), 201)]
+        public async Task<IActionResult> Post(ResourceCreateDTO resource)
+        {
+            var created = await _repository.CreateAsync(resource);
+
+            return CreatedAtRoute(nameof(Get), new { created.Id }, created);
+        }
+
+        [Authorize]
+        [HttpPut("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public async Task<Status> Put(int id, [FromBody] ResourceUpdateDTO resource)
+               => (await _repository.Update(id, resource));
+    }
+}
---
+++ VideoOverflow.Server/GlobalUsings.cs
@@ -0,0 +1,11 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Identity.Web;
+global using Microsoft.Identity.Web.Resource;
+global using System.Text.Json.Serialization;
+global using VideoOverflow.Core;
+global using VideoOverflow.Infrastructure;
+global using VideoOverflow.Server.Model;
+global using static VideoOverflow.Core.Status;
