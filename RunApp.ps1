3a8ffd6b37786a11792650f2a50a8813d6f785fa
Deniz Isik <denizisik4600@gmail.com>
2021-12-05 17:03:09 +0100

    Update RunApp.ps1

--- RunApp.ps1
+++ RunApp.ps1
@@ -2,13 +2,24 @@
 $database = "VideoOverflow"
 docker run --name postgres_db -p 5001:5432 -v postgres_data -e POSTGRES_PASSWORD=$password -e POSTGRES_DB=$database -d postgres
 $connectionString = "Host=localhost;Database=$database;Username=postgres;Password=$password"
+Set-Clipboard $connectionString
 
-Write-Host "Configuring Connection String"
+Write-Host "Setting up connectionString"
     cd .\VideoOverflow.Server\
-    dotnet user-secrets init
     dotnet user-secrets set "VideoOverflow" "$connectionString"
     
     cd..
     cd .\VideoOverflow.Infrastructure\
     dotnet ef migrations add VideoOverflowMigration
     dotnet ef database update
+    
+  Write-Host "Deleting user-secrets"  
+    cd..
+    cd .\VideoOverflow.Server\
+    dotnet user-secrets remove VideoOverflow
+    cd..
+    cd .\VideoOverflow.Infrastructure\
+     Write-Host "Deleting Migrations" 
+    rm migrations
+   
+
