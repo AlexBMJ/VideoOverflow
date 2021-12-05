27faa3617096dace62905d538cefd391aab9f492
Deniz Isik <denizisik4600@gmail.com>
2021-12-05 16:47:59 +0100

    Add script to run the application
    Make sure to be in the heal folder and run this command:
    
    .\RunApp.ps1 run

---
+++ RunApp.ps1
@@ -0,0 +1,14 @@
+﻿$password = New-Guid
+$database = "VideoOverflow"
+docker run --name postgres_db -p 5001:5432 -v postgres_data -e POSTGRES_PASSWORD=$password -e POSTGRES_DB=$database -d postgres
+$connectionString = "Host=localhost;Database=$database;Username=postgres;Password=$password"
+
+Write-Host "Configuring Connection String"
+    cd .\VideoOverflow.Server\
+    dotnet user-secrets init
+    dotnet user-secrets set "VideoOverflow" "$connectionString"
+    
+    cd..
+    cd .\VideoOverflow.Infrastructure\
+    dotnet ef migrations add VideoOverflowMigration
+    dotnet ef database update
