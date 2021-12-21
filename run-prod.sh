cfc9ef51a7e719d697ee385608c560af135c9cd3
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2021-12-22 00:19:28 +0100

    Add run script for production

---
+++ run-prod.sh
@@ -0,0 +1,8 @@
+dotnet tool install --global dotnet-ef
+dotnet tool update --global dotnet-ef
+
+cd VideoOverflow.Infrastructure
+dotnet ef migrations add VideoOverflowMigration
+
+cd ../
+sudo docker-compose up -d
